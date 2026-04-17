30dab479a0684758d347e49a0b0234fa4a8b8c74
Maliklbryant <145292679+Maliklbryant@users.noreply.github.com>
2026-04-16 21:21:13 -0400

    Replace avatar div with img element for better rendering

--- src/App.jsx
+++ src/App.jsx
@@ -7539,13 +7539,18 @@ function PublicSite({ onLogin, onConsult, onPackages }) {
             {/* Avatar */}
             <div style={{display:"flex",justifyContent:"center"}}>
               <div style={{position:"relative"}}>
-                <div className="site-about-av"
-                   style={{
-                      backgroundImage: "url('/malik.jpg')",
-                      backgroundSize: "cover",
-                      backgroundPosition: "center"
-                   }}
-                   ></div>
+                <div className="site-about-av" style={{ overflow: "hidden" }}>
+                   <img
+                      src="/malik.jpg"
+                      alt="Malik Bryant"
+                      style={{
+                         width: "100%",
+                         height: "100%",
+                         objectFit: "cover",
+                         display: "block"
+                      }}
+                   />
+                </div>
                 {/* Credential tag floating off the avatar */}
                 <div style={{
                   position:"absolute",bottom:-12,left:"50%",transform:"translateX(-50%)",
